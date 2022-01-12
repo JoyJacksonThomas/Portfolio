792c22d31ae220dfa326aecc0a0c4cd50d08b5c1
tommy <thomas.mann@mymail.champlain.edu>
2022-01-11 20:57:33 -0500

    First version of site basically done!

--- index2.js
+++ index2.js
@@ -4,7 +4,7 @@ function toggleMobileMenu() {
 	if(document.getElementById("hamburger-icon").classList.length != 0)
 	{
 		
-		document.getElementById("aboutme").style.paddingTop = "210px";
+		document.getElementById("aboutme").style.paddingTop = "170px";
 	}
 	else{
 		document.getElementById("aboutme").style.paddingTop = "0px";
@@ -15,29 +15,23 @@ function toggleMobileMenu() {
 const height = document.querySelector("#height span");
 const width = document.querySelector("#width span");
 
+var goatHeight = document.querySelector("#goatHeight span");
+
 var aboutme;
    
 // Insert values on load of page
-//window.onload = function() {
-//    height.innerHTML = window.innerHeight;
-//    width.innerHTML = window.innerWidth;
-//	aboutme = document.getElementById("aboutme");
-//};
+window.onload = function() {
+	aboutme = document.getElementById("aboutme");
+	goatHeight = document.getElementById("SuperGoatDeath").height;
+	document.getElementById("GoatDeathContainer").style.height = goatHeight;
+};
 //   
 //// Change values when window is resized
-//window.onresize = function() {
-//   
-//    // Setting the current height & width
-//    // to the elements
-//    height.innerHTML = window.innerHeight; 
-//    width.innerHTML = window.innerWidth;
-//	
-//	if(width < 600)
-//	{
-//		alert('Less than 600');
-//		aboutme.style.fontSize = "40px";
-//		//document.getElementById("aboutme").style.fontcolor = #ff3860;
-//		//document.getElementById("aboutme").style.fontSize = 30px;
-//	}
-//};
+window.onresize = function() {
+   
+	goatHeight = document.getElementById("SuperGoatDeath").height;
+	document.getElementById("GoatDeathContainer").style.height = goatHeight;
+	
+	
+};
 
